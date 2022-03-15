5ee22f808a16280d82973483e06bd453483738a4
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-03-14 21:34:20 -0500

    git latest

--- Assets/Scripts/VrManager.cs
+++ Assets/Scripts/VrManager.cs
@@ -9,11 +9,16 @@ public class VrManager : Conexion
 {
     public PlaceAtLocation placeAtLocation;
     public Transform arrowObject;
-    public TMP_InputField txt_lat_lng;
+    public TMP_Text txt_myLocation;
     public TMP_Text txt_distance;
     public TMP_Text txt_info;
     public Location locationUG = Global.GetLocationUG();
-    public LocationVr locationVr = new LocationVr();
+    // public LocationVr locationVr = new LocationVr();
+
+    public Location goLocation = null;
+    public Location goLocationSecond = null;
+    private double distance = 0;
+
 
     // public RenderPathLine renderPathLine;
 
@@ -24,37 +29,34 @@ public class VrManager : Conexion
 
     void Start()
     {
-        // StartCoroutine(getPosition());
+        StartCoroutine(getPosition());
         // path = renderPathLine.PathSettings.LocationPath;
-        string lat = "-2.1609372664849325".ToString().Replace(",", ".");
-        string lng = "-79.89925870340669".ToString().Replace(",", ".");
-        string url = $"events/{Global.getEventId()}/waypoints?lng={lng}&lat={lat}";
-        txt_info.text = $"Info: {url}";
-        HttpGet(url, ResponseApi, ResponseFail);
+        // string lat = "-2.1609372664849325".ToString().Replace(",", ".");
+        // string lng = "-79.89925870340669".ToString().Replace(",", ".");
+        // string url = $"events/{Global.getEventId()}/waypoints?lng={lng}&lat={lat}";
+        // txt_info.text = $"Info: {url}";
+        // HttpGet(url, ResponseApi, ResponseFail);
 
         // StartCoroutine(CallGetBackend(url));
     }
 
     public override void getResponse<T>(T data)
     {
-        Debug.Log("waypoints:" + data);
-        txt_info.text += "\n waypoints:" + data;
-        ResponseCustom response = JsonUtility.FromJson<ResponseCustom>(data.ToString());
-        print(response.success + " - " + response.data.start_location[0].ToString());
-        locationVr.location1 = new Location(response.data.start_location[1], response.data.start_location[0]);
-        locationVr.distance1 = Global.DistanceTo(Input.location.lastData.latitude, Input.location.lastData.longitude, locationVr.location1.latitudDouble, locationVr.location1.longitudDouble);
-        if (response.data.end_location != null)
-        {
-            locationVr.location2 = new Location(response.data.end_location[1], response.data.end_location[0]);
-        }
-        // List<ARLocation.Location> locations = new List<ARLocation.Location>();
-        // for (int i = 0; i < _location.Count; i++)
+        // Debug.Log("waypoints:" + data);
+        // txt_info.text += "\n waypoints:" + data;
+        // ResponseCustom response = JsonUtility.FromJson<ResponseCustom>(data.ToString());
+        // print(response.success + " - " + response.data.start_location[0].ToString());
+
+        // Location myLocation = new Location(Input.location.lastData.latitude, Input.location.lastData.longitude);
+        // goLocation = new Location(response.data.start_location[1], response.data.start_location[0]);
+        // locationVr.distance1 = Global.DistanceTo(Input.location.lastData.latitude, myLocation.longitudDouble, locationVr.location1.latitudDouble, locationVr.location1.longitudDouble);
+        // if (response.data.end_location != null)
         // {
-        //     locations.Add(new ARLocation.Location(_location[i][1], _location[i][0]));
+        //     locationVr.location2 = new Location(response.data.end_location[1], response.data.end_location[0]);
         // }
-        // path.Locations = locations.ToArray();
-        updatePointVr(locationVr.location1.latitudDouble, locationVr.location1.longitudDouble);
-        txt_info.text += "fist point" + placeAtLocation.Location.Latitude + "," + placeAtLocation.Location.Longitude + "second distance" + locationVr.distance2;
+        // updatePointVr(goLocation.latitudDouble, goLocation.longitudDouble);
+        // txt_info.text += $"punto del waypoint: {placeAtLocation.Location.Latitude}, {placeAtLocation.Location.Longitude}  "
+        // txt_info.text += $"mi posicion: {locationVr.distance}";
 
     }
 
@@ -64,20 +66,19 @@ public class VrManager : Conexion
         txt_info.text += "\n waypoints:" + data;
         ResponseCustom response = JsonUtility.FromJson<ResponseCustom>(data.ToString());
         print(response.success + " - " + response.data.start_location[0].ToString());
-        locationVr.location1 = new Location(response.data.start_location[1], response.data.start_location[0]);
-        locationVr.distance1 = Global.DistanceTo(Input.location.lastData.latitude, Input.location.lastData.longitude, locationVr.location1.latitudDouble, locationVr.location1.longitudDouble);
+
+        Location myLocation = getMyLocation();
+        goLocation = new Location(response.data.start_location[1], response.data.start_location[0]);
+        distance = Global.DistanceTo(myLocation.latitudDouble, myLocation.longitudDouble, goLocation.latitudDouble, goLocation.longitudDouble);
         if (response.data.end_location != null)
         {
-            print("end_location");
-            // locationVr.location2 = new Location(response.data.end_location[1], response.data.end_location[0]);
-            // ARLocation.Location[] locations = { new ARLocation.Location(locationVr.location1.latitudDouble, locationVr.location1.longitudDouble), new ARLocation.Location(locationVr.location2.latitudDouble, locationVr.location2.longitudDouble) };
-            // path.Locations = locations;
+            goLocationSecond = new Location(response.data.end_location[1], response.data.end_location[0]);
         }
-        updatePointVr(locationVr.location1.latitudDouble, locationVr.location1.longitudDouble);
-
-
-
-        txt_info.text += "\n point" + placeAtLocation.Location.Latitude + "," + placeAtLocation.Location.Longitude + " distance" + locationVr.distance2;
+        updatePointVr(goLocation.latitudDouble, goLocation.longitudDouble);
+        txt_distance.text = distance.ToString();
+        txt_info.text += $"punto del waypoint: {placeAtLocation.Location.Latitude}, {placeAtLocation.Location.Longitude}";
+        txt_myLocation.text += $"mi posición: {myLocation.latitud}, {myLocation.longitud}";
+        // txt_info.text += "\n point" + placeAtLocation.Location.Latitude + "," + placeAtLocation.Location.Longitude + " distance" + locationVr.distance2;
     }
 
     public void ResponseFail(string data)
@@ -93,7 +94,6 @@ public class VrManager : Conexion
 
         if (!Input.location.isEnabledByUser)
         {
-            print("la localizacion esta desabilitada");
             NotificationController.ShowToast("Ubicación deshabilitada");
             yield break;
         }
@@ -107,14 +107,11 @@ public class VrManager : Conexion
             yield return new WaitForSeconds(1);
             maxWait--;
         }
-
         // If the service didn't initialize in 20 seconds this cancels location service use.
         if (maxWait < 1)
         {
-            //print("Timed out");
             yield break;
         }
-
         // If the connection failed this cancels location service use.
         if (Input.location.status == LocationServiceStatus.Failed)
         {
@@ -127,15 +124,14 @@ public class VrManager : Conexion
             // If the connection succeeded, this retrieves the device's current location and displays it in the Console window.
             string message = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
             NotificationController.ShowToast(message);
-            string lat = Input.location.lastData.latitude.ToString().Replace(",", ".");
-            string lng = Input.location.lastData.longitude.ToString().Replace(",", ".");
-            string url = $"events/{Global.getEventId()}/waypoints?lng={lng}&lat={lat}";
-            txt_info.text = $"Info: {url}";
+            Location myLocation = getMyLocation();
+            string url = $"events/{Global.getEventId()}/waypoints?lng={myLocation.longitud}&lat={myLocation.latitud}";
+            txt_info.text = $"Info url: {url}";
             HttpGet(url, ResponseApi, ResponseFail);
             // StartCoroutine(CallGetBackend(url));
         }
         // Stops the location service if there is no need to query location updates continuously.
-        Input.location.Stop();
+        // Input.location.Stop();
     }
 
     public void updatePointVr(double lat, double lng, double alt = 0)
@@ -150,35 +146,43 @@ public class VrManager : Conexion
         placeAtLocation.Location = newLocation;
     }
 
-    private ARLocation.Location generateArLocation(double lat, double lng)
-    {
-        return new ARLocation.Location()
-        {
-            Latitude = lat,
-            Longitude = lng,
-            Altitude = 0,
-            AltitudeMode = AltitudeMode.DeviceRelative
-        };
+    // private ARLocation.Location generateArLocation(double lat, double lng)
+    // {
+    //     return new ARLocation.Location()
+    //     {
+    //         Latitude = lat,
+    //         Longitude = lng,
+    //         Altitude = 0,
+    //         AltitudeMode = AltitudeMode.DeviceRelative
+    //     };
+    // }
 
+    private Location getMyLocation()
+    {
+        double latitude = Input.location.lastData.latitude;
+        double longitude = Input.location.lastData.longitude;
+        return new Location(latitude, longitude);
     }
 
     private void Update()
     {
-        double latitude = Input.location.lastData.latitude;
-        double longitude = Input.location.lastData.longitude;
-
-        double latitudeGo = locationVr.location1.latitudDouble;
-        double longitudeGo = locationVr.location1.longitudDouble;
-
-        float bearing = angleFromCoordinate(latitude, longitude, latitudeGo, longitudeGo);
+        Location myLocation = getMyLocation();
+        float bearing = angleFromCoordinate(myLocation.latitudDouble, myLocation.longitudDouble, goLocation.latitudDouble, goLocation.longitudDouble);
         arrowObject.rotation = Quaternion.Slerp(arrowObject.rotation, Quaternion.Euler(0, Input.compass.magneticHeading + bearing, 0), 100f);
-        // print("coordenadas"+Input.location.lastData.latitude+" - "+ Input.location.lastData.longitude);
-        locationVr.distance1 = Global.DistanceTo(latitude, longitude, latitudeGo, longitudeGo);
-        if (locationVr.distance1 < 5)
+        distance = Global.DistanceTo(myLocation.latitudDouble, myLocation.longitudDouble, goLocation.latitudDouble, goLocation.longitudDouble);
+        if (distance < 3)
         {
+            distance = 1000;
             StartCoroutine(getPosition());
+            if(goLocationSecond != null)
+            {
+                goLocation = goLocationSecond;
+                goLocationSecond = null;
+            }
+            
         }
-        txt_distance.text = $"Distancia para el waypoint: {Global.DistanceTo(Input.location.lastData.latitude, Input.location.lastData.longitude, locationVr.location1.latitudDouble, locationVr.location1.longitudDouble)} m";
+        txt_distance.text = distance.ToString();
+        txt_myLocation.text = $" {myLocation.latitud}, {myLocation.longitud}";
     }
 
     private float angleFromCoordinate(double _lat1, double _long1, double _lat2, double _long2)
@@ -202,22 +206,22 @@ public class VrManager : Conexion
         return brng;
     }
 
-    public void newLocationGo()
-    {
-        string stringLatLng = txt_lat_lng.text;
-        string[] latLng = stringLatLng.Split(',');
-        locationUG = new Location(double.Parse(latLng[0]), double.Parse(latLng[1]));
-    }
+    // public void newLocationGo()
+    // {
+    //     string stringLatLng = txt_lat_lng.text;
+    //     string[] latLng = stringLatLng.Split(',');
+    //     locationUG = new Location(double.Parse(latLng[0]), double.Parse(latLng[1]));
+    // }
 }
 
-[System.Serializable]
-public class LocationVr
-{
-    public Location location1;
-    public Location location2;
-    public double distance1;
-    public double distance2;
-}
+// [System.Serializable]
+// public class LocationVr
+// {
+//     public Location location1;
+//     public Location location2;
+//     public double distance1;
+//     public double distance2;
+// }
 [System.Serializable]
 public class ResponseCustom
 {
